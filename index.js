e612003e3390c39582e1c7c985ad71f68a8c8f96
Elena <inculet98@mail.ru>
2023-09-25 16:24:17 +0300

    adaugare modificari

---
+++ index.js
@@ -0,0 +1,93 @@
+// let str = "str"
+// let str1 = "str"
+
+// console.log(str === str1)
+
+// let obj = {
+//   nume: "Elena"
+// }
+// let obj1 = {
+//   nume: "Elena"
+// }
+// let str = "straaaaaing"
+// console.log(str)
+// Number
+// String
+// Boolean
+// NaN
+// undefined
+// null
+
+
+// const obj{
+//   nume: "Elena",
+//   lastName: "last name",
+// }
+// const arr = { 1, 2, 4, true, "stringuri"}
+
+
+
+// var variabila = {
+//   number: 3
+// }
+// var culoare = "rosu"
+// var culoare = adresa unde este pastrata culoarea
+
+// console.log(variabila)
+
+// console.log(3 / 3)
+
+// console.log(variabila)
+// var variabila = 5
+// let variabila1 = 6
+// const constanta = 7
+
+// variabila = 6
+// variabila1 = "text"
+// constanta = "alta valoare"
+
+// console.log(variabila)
+
+
+// const variaBila = 5
+// const variabila = 6
+// console.log(variabila == variaBila)
+
+// const numb = 44
+// const str = 'un string'
+// const bool = false
+// const undef = undefined
+// const faraValoare = null
+// const obj = {}
+// // console.log(typeof numb)
+// // console.log(typeof str)
+// // console.log(typeof bool)
+// // console.log(typeof undef)
+// console.log(typeof faraValoare)
+// console.log(typeof obj === 'object' && obj === null)
+
+
+// const numb = 44
+// console.log(numb + numb)
+
+// const numb = 44
+
+// console.log('3' + 3)
+// console.log(NaN === NaN)
+
+const string = 'text fhfvdv fcjdfvkf /// fihbdv';
+const numb = 4;
+const bool = Boolean;
+const undef = undefined;
+const symb = Symbol;
+const bigNumber = BigInt;
+const faraValoare = null;
+
+const obj = { nume: 'Elena' }
+const arr = { 1, 2, 3, 4 }
+
+console.log(obj)
+
+obj.hobby = 'swiming'
+
+console.log(obj)
